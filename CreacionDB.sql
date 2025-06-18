2e26c712d61fc973d53feec86eea95d4f0c3440a
FrancoCapurro <79758145+FrancoCapurro@users.noreply.github.com>
2025-06-18 13:29:24 -0300

    Agrego el aporte perdido de Rosa, + fixes

--- CreacionDB.sql
+++ CreacionDB.sql
@@ -7,6 +7,7 @@ IDMarca int primary key not null identity(1,1),
 NombreMarca varchar(50) not null unique
 
 )
+GO
 
 
 Create table Categoria (
@@ -14,6 +15,7 @@ IDCategoria int primary key not null identity(1,1),
 NombreCategoria varchar(50) not null unique
 
 )
+GO
 
 
 Create table Producto(
@@ -27,6 +29,7 @@ IDCategoria int not null foreign key references Categoria(IDCategoria),
 IDMarca int not null foreign key references Marca(IDMarca)
 
 )
+GO
 
 
 Create table Proveedor (
@@ -38,40 +41,83 @@ Email varchar(50) not null unique
 
 )
 
+GO
+
 Create table Cliente (
     IDCliente int primary key identity(1,1),
     Nombre varchar(50) not null,
     Apellido varchar(50) not null,
-    Telefono varchar(20)
+    Telefono varchar(20) not null
 )
 
+GO
 Create table FormaDePago (
     IDFormaDePago TINYINT primary key identity(1,1),
     Pago varchar(50) not null
 )
+GO
 
 Create table Venta (
     IDVenta int primary key identity(1,1),
     FechaVenta date not null,
-    IDCliente int null,
-    IDFormaDePago tinyint not null,
-    TotalVenta decimal(10,2),
-    foreign key references Cliente(IDCliente),
-    foreign key references FormaDePago(IDFormaDePago)
+    TotalVenta decimal(10,2) not null CHECK(TotalVenta>0),
+	IDCliente int null foreign key references Cliente(IDCliente),
+	IDFormaDePago TINYINT not null foreign key references FormaDePago(IDFormaDePago)
 )
+GO
 
 Create table DetalleVenta (
-    IDVenta int not null,
-    IDProducto int not null,
     CantidadVenta int not null check (CantidadVenta > 0),
     PrecioUnitario decimal(10,2) not null check (PrecioUnitario >= 0),
     SubTotalVenta decimal(10,2) not null check(SubTotalVenta >= 0),
-    primary key (IDVenta, IDProveedor),
-    foreign key references Venta (IDVenta),
-    foreign key references Producto(IDProducto)
+	IDVenta int not null foreign key references Venta(IDVenta),
+	IDProducto INT not null foreign key references Producto(IDProducto),
+    primary key (IDVenta,IDProducto),
+)
+GO
+
+CREATE TABLE Compra (
+    IDCompra int not null PRIMARY KEY IDENTITY (1,1),
+    FechaCompra DATE NOT NULL,
+    IDFormaDePago TINYINT not null FOREIGN KEY REFERENCES FormaDePago(IDFormaDePago),
+    IDProveedor INTEGER NOT NULL FOREIGN KEY REFERENCES Proveedor (IDProveedor),
+    TotalCompra DECIMAL (10, 2) NOT NULL CHECK(TotalCompra>0)
+)
+GO
+CREATE TABLE DetalleCompra(
+    IDCompra int not NULL FOREIGN KEY REFERENCES Compra (IDCompra),
+    IDProducto int not NULL FOREIGN KEY REFERENCES Producto(IDProducto),
+    CantidadCompra int not null CHECK (CantidadCompra>0),
+    CostoUnitario decimal (10,2) not null CHECK(CostoUnitario>=0),
+    SubTotalCompra decimal (10,2) not null CHECK(SubTotalCompra>0),
+    PRIMARY key (IDCompra, IDProducto)
+)
+GO
+
+
+CREATE TABLE  ProveedorProducto(
+    IDProveedor INT NOT NULL FOREIGN KEY REFERENCES Proveedor(IDProveedor),
+    IDProducto INT NOT NULL FOREIGN KEY REFERENCES Producto(IDProducto),
+    PrecioRef DECIMAL (10,2) NOT NULL CHECK (PrecioRef>0), 
+    PRIMARY KEY (IDProveedor, IDProducto)
 )
 
+GO
 
+CREATE TABLE TipoMovimiento (
+    IDTipoMovimiento TINYINT not null PRIMARY KEY IDENTITY (1,1),
+    NombreMovimiento VARCHAR (50) not null,
+    Motivo VARCHAR (50) not null
+)
+GO
+
+CREATE TABLE MovimientoInventario(
+    IDMovimiento INTEGER NOT NULL PRIMARY KEY IDENTITY (1,1),
+    IDProducto INTEGER NOT NULL FOREIGN KEY REFERENCES Producto(IDProducto),
+    IDTipoMovimiento TINYINT NOT NULL FOREIGN KEY REFERENCES TipoMovimiento (IDTipoMovimiento),
+    Cantidad INTEGER NOT NULL CHECK (Cantidad>0),
+    FechaMovimiento DATE NOT NULL
+	)
 
 
 
