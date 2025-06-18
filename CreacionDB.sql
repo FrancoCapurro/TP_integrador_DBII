2ae753b993b5ab36bbe240c6e7804d907cdfea66
Ludmila Leiva <leivaludmila99@gmail.com>
2025-06-18 01:45:24 -0300

    Agrego modificacion de las Tablas Venta y DetalleVenta

--- CreacionDB.sql
+++ CreacionDB.sql
@@ -55,65 +55,23 @@ Create table Venta (
     FechaVenta date not null,
     IDCliente int null,
     IDFormaDePago tinyint not null,
+    TotalVenta decimal(10,2),
     foreign key references Cliente(IDCliente),
     foreign key references FormaDePago(IDFormaDePago)
 )
 
 Create table DetalleVenta (
-    IDDetalleVenta int primary key identity(1,1),
     IDVenta int not null,
     IDProducto int not null,
-    CantidadVenta int not null,
-    PrecioUnitario decimal(10,2) not null,
-    SubTotal as (CantidadVenta * PrecioUnitario) persisted,
+    CantidadVenta int not null check (CantidadVenta > 0),
+    PrecioUnitario decimal(10,2) not null check (PrecioUnitario >= 0),
+    SubTotalVenta decimal(10,2) not null check(SubTotalVenta >= 0),
+    primary key (IDVenta, IDProveedor),
     foreign key references Venta (IDVenta),
     foreign key references Producto(IDProducto)
 )
-CREATE TABLE Compra (
-    IDCompra int not null PRIMARY KEY IDENTITY (1,1),
-    FechaCompra DATE NOT NULL,
-    IDFormaDePago TINYINT not null FOREIGN KEY REFERENCES FormaDePago(IDFormaDePago),
-    IDProveedor INTEGER NOT NULL FOREIGN KEY REFERENCES Proveedor (IDProveedor),
-    TotalCompra DECIMAL (10, 2) NOT NULL CHECK(TotalCompra>0)
-)
-GO
-CREATE TABLE DetalleCompra(
-    IDCompra int not NULL FOREIGN KEY REFERENCES Compra (IDCompra),
-    IDProducto int not NULL FOREIGN KEY REFERENCES Producto(IDProducto),
-    CantidadCompra int not null CHECK (CantidadCompra>0),
-    CostoUnitario decimal (10,2) not null CHECK(CostoUnitario>=0),
-    SubTotalCompra decimal (10,2) not null CHECK(SubTotalCompra>0),
-    PRIMARY key (IDCompra, IDProducto)
-)
-GO 
-
-
-CREATE TABLE  ProveedorProducto(
-    IDProveedor INT NOT NULL FOREIGN KEY REFERENCES Proveedor(IDProveedor),
-    IDProducto INT NOT NULL FOREIGN KEY REFERENCES Producto(IDProducto),
-    PrecioRef DECIMAL (10,2) NOT NULL CHECK (PrecioRef>0), 
-    PRIMARY KEY (IDProveedor, IDProducto)
-)
-GO
-
-CREATE TABLE TipoMovimiento (
-    IDTipoMovimiento TINYINT not null PRIMARY KEY IDENTITY (1,1),
-    NombreMovimiento VARCHAR (50) not null,
-    Motivo VARCHAR (50) not null,
-
-)
-GO
-
-CREATE TABLE MovimientoInventario(
-    IDMovimiento INTEGER NOT NULL PRIMARY KEY IDENTITY (1,1),
-    IDProducto INTEGER NOT NULL FOREIGN KEY REFERENCES Producto(IDProducto),
-    IDTipoMovimiento TINYINT NOT NULL FOREIGN KEY REFERENCES TipoMovimiento (IDTipoMovimiento),
-    Cantidad INTEGER NOT NULL CHECK (Cantidad>0),
-    FechaMovimiento DATE NOT NULL
-
-)
 
 
 
- 
+
 
